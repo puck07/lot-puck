81e703a97950313e9947716d1a66e8ec9dc5cf32
puck <36668@ma-web.nl>
2024-03-05 10:47:56 +0100

    add\

---
+++ code/les 2/sketch_mar05a/sketch_mar05a.ino
@@ -0,0 +1,31 @@
+void setup() {
+  Serial.begin(115200);
+
+}
+
+void loop() {
+ Serial.println("Hallo!");
+
+ int a = random(999);
+ int b = random(999);
+ int c = random(999);
+ int d = random(999);
+}
+
+if (a > b)
+  digitalWrite(D3, HIGH);
+  delay(10000);
+  digitalWrite(D3, LOW);
+
+if (c > a < c)
+  digitalWrite(D5, HIGH);
+  delay(10000);
+  digitalWrite(D5, LOW);
+
+if ()
+  digitalWrite(D6, HIGH);
+  delay(10000);
+  digitalWrite(D6, LOW);
+  
+  
+}
---
+++ code/sketch_mar05a/sketch_mar05a.ino
@@ -0,0 +1,40 @@
+void setup () {
+  pinMode(D3, OUTPUT);;
+  } 
+
+void loop() {
+  digitalWrite(D3, HIGH);
+  delay(500);
+  digitalWrite(D3, LOW);
+  delay(250);
+
+   digitalWrite(D3, HIGH);
+  delay(1500);
+  digitalWrite(D3, LOW);
+  delay(250);
+
+  digitalWrite(D3, HIGH);
+  delay(1500);
+  digitalWrite(D3, LOW);
+  delay(250);
+
+  digitalWrite(D3, HIGH);
+  delay(1500);
+  digitalWrite(D3, LOW);
+  delay(250);
+
+  digitalWrite(D3, HIGH);
+  delay(500);
+  digitalWrite(D3, LOW);
+  delay(500);
+
+  digitalWrite(D3, HIGH);
+  delay(500);
+  digitalWrite(D3, LOW);
+  delay(250);
+
+  digitalWrite(D3, HIGH);
+  delay(1500);
+  digitalWrite(D3, LOW);
+  delay(250);
+}
